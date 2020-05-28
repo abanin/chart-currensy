5656154419bfd11d6028e1cfe70b7cbd25dc7d82
Aleksandr Abanin <abaninsash@gmail.com>
2020-05-28 11:47:16 +0300

    Updates

---
+++ precache-manifest.459af60ad680b7a2ea904693d7991a36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e9691eb11cac2854a0859c92fd54a8",
+    "url": "/chart-currensy/index.html"
+  },
+  {
+    "revision": "fd7ac30678c47300429f",
+    "url": "/chart-currensy/static/css/main.80a2cf2c.chunk.css"
+  },
+  {
+    "revision": "53c035e67cc644ea32b1",
+    "url": "/chart-currensy/static/js/2.e5f61a6e.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/chart-currensy/static/js/2.e5f61a6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd7ac30678c47300429f",
+    "url": "/chart-currensy/static/js/main.5b5159a8.chunk.js"
+  },
+  {
+    "revision": "d8ae0cfd0615bd40672f",
+    "url": "/chart-currensy/static/js/runtime-main.b73500de.js"
+  }
+]);
